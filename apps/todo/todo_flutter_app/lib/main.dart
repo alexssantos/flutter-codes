d387aa29c7b62e7d237b64010fe2a05e648eddc8
Alex Santos <arkanjo.alex@gmail.com>
2019-12-30 11:26:35 -0300

    Dart basics

--- apps/todo/todo_flutter_app/lib/main.dart
+++ apps/todo/todo_flutter_app/lib/main.dart
@@ -4,63 +4,63 @@ import 'package:todo_flutter_app/models/Tarefa.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Todo app",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.purpleAccent),
-      home: HomePage(),
-    );
-  }
+  	@override
+  	Widget build(BuildContext context) {
+		return MaterialApp(
+	  		title: "Todo app",
+			debugShowCheckedModeBanner: false,
+			theme: ThemeData(primaryColor: Colors.purpleAccent),
+			home: HomePage(),
+		);
+  	}
 }
 
 class HomePage extends StatefulWidget {
-  var tarefas = new List<Tarefa>();
+  	var tarefas = new List<Tarefa>();
 
-  HomePage() {
-    tarefas = [];
-    tarefas.add(Tarefa(title: "Tarefa 1", done: false));
-    tarefas.add(Tarefa(title: "Tarefa 2", done: true));
-    tarefas.add(Tarefa(title: "Tarefa 3", done: false));
-  }
+	HomePage() {
+		tarefas = [];
+		tarefas.add(Tarefa(title: "Tarefa 1", done: false));
+		tarefas.add(Tarefa(title: "Tarefa 2", done: true));
+		tarefas.add(Tarefa(title: "Tarefa 3", done: false));
+	}
 
-  @override
-  _HomePageState createState() => _HomePageState();
+	@override
+	_HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Todo List Title AppBar"),
-      ),
-      body: ListView.builder(
-        itemCount: widget.tarefas.length,
-        itemBuilder: (BuildContext ctx, int index) {
-          final item = widget.tarefas[index];
-          return CheckboxListTile(
-            title: Text(item.title),
-            key: Key(item.title),
-            value: item.done,
-            onChanged: (value) {
-              // ! Precisa rerenderizar mas não pode chamar daqui o metodo builder()
-              setState(() {
-                item.done = value;
-              });
-            },
-          );
-        },
-      ),
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text("Todo List Title AppBar"),
+			),
+			body: ListView.builder(
+				itemCount: widget.tarefas.length,
+				itemBuilder: (BuildContext ctx, int index) {
+					
+					final item = widget.tarefas[index];
+					
+					return CheckboxListTile(
+						title: Text(item.title),
+						key: Key(item.title),
+						value: item.done,
+						onChanged: (value) {
+						// ! Precisa rerenderizar mas não pode chamar daqui o metodo builder()
+						setState(() => item.done = value);
+						},
+					);
+				},
+			),
+		);
+	}
 }
 
 BoxDecoration myBoxDecoration() {
-  return BoxDecoration(
-    border: Border.all(
-      width: 2,
-    ),
-  );
+	return BoxDecoration(
+		border: Border.all(
+			width: 2
+		),
+	);
 }
--- apps/todo/todo_flutter_app/lib/models/Tarefa.dart
+++ apps/todo/todo_flutter_app/lib/models/Tarefa.dart
@@ -1,19 +1,19 @@
 class Tarefa {
-  String title;
-  bool done;
+    String title;
+    bool done;
 
-  Tarefa({this.title, this.done});
+    Tarefa({this.title, this.done});
 
-  //MAPPER JSON
-  Tarefa.fromJson(Map<String, dynamic> json) {
-    title = json['title'];
-    done = json['done'];
-  }
+    //MAPPER JSON
+    Tarefa.fromJson(Map<String, dynamic> json) {
+        title = json['title'];
+        done = json['done'];
+    }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['title'] = this.title;
-    data['done'] = this.done;
-    return data;
-  }
+    Map<String, dynamic> toJson() {
+        final Map<String, dynamic> data = new Map<String, dynamic>();
+        data['title'] = this.title;
+        data['done'] = this.done;
+        return data;
+    }
 }

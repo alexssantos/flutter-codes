eb00068ed86b0e7811e3681fd24ac7176516339c
Alex Santos <arkanjo.alex@gmail.com>
2020-01-04 16:11:23 -0300

    ADD models Event & Guest

---
+++ apps/events_app/lib/models/guest.dart
@@ -0,0 +1,14 @@
+class Guest {
+	final String imagePath;
+
+	Guest({this.imagePath});  
+}
+
+final guests = [
+	Guest(imagePath: "assets/guest_imgs/guest1.jpg"),
+	Guest(imagePath: "assets/guest_imgs/guest2.jpg"),
+	Guest(imagePath: "assets/guest_imgs/guest3.jpg"),
+	Guest(imagePath: "assets/guest_imgs/guest4.jpg"),
+	Guest(imagePath: "assets/guest_imgs/guest5.jpg"),
+	Guest(imagePath: "assets/guest_imgs/guest6.jpg"),
+];

2127f461d60494f6a918561ae64b32810504054d
Alex Santos <arkanjo.alex@gmail.com>
2020-01-05 10:50:28 -0300

    Home Page - Top Itens layout

--- apps/events_app/lib/ui/pages/home-page.dart
+++ apps/events_app/lib/ui/pages/home-page.dart
@@ -1,3 +1,4 @@
+import 'package:events_app/styleguide.dart';
 import 'package:events_app/ui/pages/home-page-background.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +8,35 @@ class HomePage extends StatelessWidget {
 		return Scaffold(
 			body: Stack(
 				children: <Widget>[
-					HomePageBackground(screenHeight: MediaQuery.of(context).size.height)
+					HomePageBackground(screenHeight: MediaQuery.of(context).size.height),
+
+					SafeArea(
+						child: SingleChildScrollView(
+							child: Column(
+								crossAxisAlignment: CrossAxisAlignment.start,
+								children: <Widget>[
+									
+									//TOP ITENS 
+									Padding(
+										padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 22.0),
+										child: Row(
+											children: <Widget>[
+												Text("LOCAL EVENTS", style: fadedTextStyle,),
+												Spacer(),
+												Icon(
+													Icons.person_outline,
+													color: Color(0x99FFFFFF),
+													size: 30,
+												)
+											],
+										)
+									)
+
+
+								],
+							),
+						),
+					)
 				],
 			),
 		
